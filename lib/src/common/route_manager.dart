3a50b7d6f5a24aa7ca40d7195e7667d6582aed87
unisha21 <unishatamang02@gmail.com>
2025-05-12 00:38:15 +0545

    feat: update route manager to include service detail and support screens

--- lib/src/common/route_manager.dart
+++ lib/src/common/route_manager.dart
@@ -6,7 +6,11 @@ import 'package:homestay_host/src/features/dashboard/screens/home_screen.dart';
 import 'package:homestay_host/src/features/homestay/domain/models/homestay_model.dart';
 import 'package:homestay_host/src/features/homestay/screens/create_listing.dart';
 import 'package:homestay_host/src/features/homestay/screens/listing_screen.dart';
+import 'package:homestay_host/src/features/homestay/screens/service_detail_screen.dart';
 import 'package:homestay_host/src/features/homestay/screens/update_homestay.dart';
+import 'package:homestay_host/src/features/profile/screens/profile_edit_screen.dart';
+import 'package:homestay_host/src/features/profile/screens/profile_screen.dart';
+import 'package:homestay_host/src/features/profile/screens/support_screen.dart';
 
 class Routes {
   static const String splashRoute = '/';
@@ -14,6 +18,7 @@ class Routes {
   static const String registerRoute = '/register';
   static const String homeRoute = '/home';
   static const String createListingRoute = '/create-listing';
+  static const String serviceDetailRoute = '/service-detail';
   static const String myListingRoute = '/my-listing';
   static const String updateListingRoute = '/update-listing';
   static const String notificationRoute = '/notification';
@@ -38,18 +43,25 @@ class RouteGenerator {
       case Routes.homeRoute:
         return MaterialPageRoute(builder: (_) => const HomeScreen());
       case Routes.createListingRoute:
-        return MaterialPageRoute(
-          builder: (_) => const CreateListingScreen(),
-        );
+        return MaterialPageRoute(builder: (_) => const CreateListingScreen());
       case Routes.myListingRoute:
+        return MaterialPageRoute(builder: (_) => const ListingScreen());
+      case Routes.serviceDetailRoute:
+        final args = settings.arguments as HomestayModel;
         return MaterialPageRoute(
-          builder: (_) => const ListingScreen(),
+          builder: (_) => ServiceDetailScreen(args),
         );
       case Routes.updateListingRoute:
         final args = settings.arguments as HomestayModel;
         return MaterialPageRoute(
-          builder: (_) => UpdateHomestayScreen(homestay: args,),
+          builder: (_) => UpdateHomestayScreen(homestay: args),
         );
+      case Routes.profileRoute:
+        return MaterialPageRoute(builder: (_) => const ProfileScreen());
+      case Routes.profileEditRoute:
+        return MaterialPageRoute(builder: (_) => const ProfileEditScreen());
+      case Routes.supportRoute:
+        return MaterialPageRoute(builder: (_) => const SupportScreen());
       default:
         return unDefinedRoute();
     }
