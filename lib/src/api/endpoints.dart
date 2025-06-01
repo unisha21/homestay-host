df5061ebb307ee35ae1a53c90f30abf49c5f7d10
unisha21 <unishatamang02@gmail.com>
2025-06-01 15:13:40 +0545

    feat: update notification API endpoints and refactor sendNotification method

--- lib/src/api/endpoints.dart
+++ lib/src/api/endpoints.dart
@@ -1,3 +1,4 @@
 class ApiEndPoints {
-  static const String baseNotificationUrl = 'https://fcm.googleapis.com/fcm/send';
+  static const String baseUrl = 'http://10.0.2.2:8000';
+  static const String sendNotification = '/api/send-notification';
 }
--- lib/src/features/notification/data/notification_datasource.dart
+++ lib/src/features/notification/data/notification_datasource.dart
@@ -1,77 +1,76 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:homestay_host/src/api/api_keys.dart';
 import 'package:homestay_host/src/api/endpoints.dart';
 
-class NotificationDataSource{
-  final _notificationDb = FirebaseFirestore.instance.collection('notifications');
+class NotificationDataSource {
+  final _notificationDb = FirebaseFirestore.instance.collection(
+    'notifications',
+  );
 
-  Stream<List<NotificationModel>> getNotifications(){
+  Stream<List<NotificationModel>> getNotifications() {
     final uid = FirebaseAuth.instance.currentUser!.uid;
-    try{
-      final data =  _notificationDb.where('receiverId', isEqualTo: uid).snapshots();
+    try {
+      final data =
+          _notificationDb.where('receiverId', isEqualTo: uid).snapshots();
       final response = data.asyncMap((event) async {
-        final data = Future.wait(event.docs.map((e) async {
-          final json = e.data();
-          return NotificationModel.fromJson({
-            ...json,
-            'notificationId': e.id,
-          });
-        }).toList());
+        final data = Future.wait(
+          event.docs.map((e) async {
+            final json = e.data();
+            return NotificationModel.fromJson({
+              ...json,
+              'notificationId': e.id,
+            });
+          }).toList(),
+        );
         return data;
       });
       return response;
-    }on FirebaseException catch(e){
+    } on FirebaseException catch (e) {
       throw Exception(e.message);
     }
   }
 
-  Future<String> updateNotificationReadStatus(String notificationId) async{
-    try{
-      await _notificationDb.doc(notificationId).update({
-        'isRead': true
-      });
+  Future<String> updateNotificationReadStatus(String notificationId) async {
+    try {
+      await _notificationDb.doc(notificationId).update({'isRead': true});
       return 'Notification read status updated';
-    }on FirebaseException catch(e){
+    } on FirebaseException catch (e) {
       throw Exception(e.message);
     }
   }
 
-  Future<void> sendNotification({required String token, required String title, required String message, Map<String, dynamic>? notificationData}) async {
+  Future<void> sendNotification({
+    required String token,
+    required String title,
+    required String message,
+    Map<String, dynamic>? notificationData,
+  }) async {
     final dio = Dio(
-        BaseOptions(
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'key=$serverKey',
-          },
-          baseUrl: ApiEndPoints.baseNotificationUrl,
-        )
+      BaseOptions(
+        headers: {'Content-Type': 'application/json'},
+        baseUrl: ApiEndPoints.baseUrl,
+      ),
     );
-    try{
+    try {
       await dio.post(
-          ApiEndPoints.baseNotificationUrl,
-          data: {
-            "to": token,
-            "priority": "High",
-            "default_notification_channel_id": "high_importance_channel",
-            "notification":{
-              "body": message,
-              "title": title,
-            },
-            "data": notificationData,
-          }
+        ApiEndPoints.sendNotification,
+        data: {
+          "token": token,
+          "title": title,
+          "body": message,
+          "data": notificationData ?? <String, dynamic>{},
+        },
       );
-    }on FirebaseException catch (err){
+    } on DioException catch (e) {
+      throw Exception(e.message);
+    } on FirebaseException catch (err) {
       throw Exception(err.message);
     }
   }
-
 }
 
-
-
-class NotificationModel{
+class NotificationModel {
   final String notificationId;
   final String title;
   final String body;
@@ -91,24 +90,24 @@ class NotificationModel{
     required this.notificationType,
     required this.senderId,
     required this.receiverId,
-    required this.isRead
+    required this.isRead,
   });
 
-  factory NotificationModel.fromJson(Map<String, dynamic> json){
+  factory NotificationModel.fromJson(Map<String, dynamic> json) {
     return NotificationModel(
-        notificationId: json['notificationId'] as String,
-        title: json['title'] as String,
-        body: json['body'] as String,
-        createdAt: json['createdAt'] as String,
-        notificationData: json['notificationData'] as Map<String, dynamic>? ?? {},
-        notificationType: json['notificationType'] as String,
-        senderId: json['senderId'] as String,
-        receiverId: json['receiverId'] as String,
-        isRead: json['isRead'] as bool
+      notificationId: json['notificationId'] as String,
+      title: json['title'] as String,
+      body: json['body'] as String,
+      createdAt: json['createdAt'] as String,
+      notificationData: json['notificationData'] as Map<String, dynamic>? ?? {},
+      notificationType: json['notificationType'] as String,
+      senderId: json['senderId'] as String,
+      receiverId: json['receiverId'] as String,
+      isRead: json['isRead'] as bool,
     );
   }
 
-  Map<String, dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     return {
       'title': title,
       'body': body,
@@ -117,7 +116,7 @@ class NotificationModel{
       'notificationType': notificationType,
       'senderId': senderId,
       'receiverId': receiverId,
-      'isRead': isRead
+      'isRead': isRead,
     };
   }
 }
